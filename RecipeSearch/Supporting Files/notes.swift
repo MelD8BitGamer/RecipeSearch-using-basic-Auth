fea5212a069b7e88eec5040c79aba0aa3b4bb1d1
melinda <melindad_82@msn.com>
2019-12-10 11:43:00 -0500

    Initial Commit

---
+++ RecipeSearch/Supporting Files/notes.swift
@@ -0,0 +1,17 @@
+//
+//  notes.swift
+//  RecipeSearch
+//
+//  Created by Melinda Diaz on 12/10/19.
+//  Copyright © 2019 Alex Paul. All rights reserved.
+//
+
+import Foundation
+
+
+//API keys are used for extra security.
+/* you only have 5 minutes and it will only accept a certain amount of users to the API
+ everyone has their own API key associated to the account. if you so more than 1000 request the n it is going cost you money and they all have limitations per month or request per time. If you do disobey the limitations you will get an error code. So this will give the vendor you usage of the API and who is using it
+ if you push your API key you will want to use ignore so that it could IGNORE your API key. If you push it a=then anyone can just goand take your API key causing you to run your API limitations
+ also if you make money off the API they are gonna wanna make money from you
+ OAuth (read more about that) its more work */
